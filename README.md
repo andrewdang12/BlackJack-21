cc862b4bafb5c00a9009a41641c37ae64edc33a7
andrewdang12 <andrewdang12@gmail.com>
2020-11-04 12:58:13 -0800

    some js redone

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # BlackJack-21
 SEI Project 01
+Here I am creating a BlackJack-21 game where you start with two cards to beat the dealer and get closer to 21
+## How to Play
+-index style (table of contents)
++ simple card flip
++ background music
+??? bet button 
++ play game button
++ Hit me button
++ stay button
++ double down 
++ split button
++ BlackJack header title
++ the cards
++ background table
+
+### Soures
+links 
+images
+names
+audio
+nairobi- theme style star wars deck
---
+++ js/main.js
@@ -0,0 +1,28 @@
+/* ----- constants -----*/
+const numbers = [2,3,4,5,6,7,8,9,10, 'K', 'Q', 'J', 'A']
+const suits = ['S', 'H', 'C', 'D']
+
+
+/*---- app's state (variables)------ */
+let cards;
+
+
+
+/*----  cached element references ----- */
+const dealerEl = document.querySelector('#dcards');
+const playerEl = document.querySelector('#pcards');
+const playEl = document.querySelector('#play');
+const hitEl = document.querySelector('#hit');
+const playagEl = document.querySelector('#playag')
+
+/* ----- event listeners ----- */
+
+playEl.addEventListener('click', );
+hitEl.addEventListener('click', );
+playagEl.addEventListener('click', );
+
+/* ----- functions ------ */
+init();
+
+
+
